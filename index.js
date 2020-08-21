7052a2a4dc4688a7c5225c8a403d4d5f0dc88c1d
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-08-21 12:39:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace('px', '');
   let left = parseInt(leftNumbers, 10)
   
-  if left > 0) {
+  if (left > 0) {
     dodger.style.left = `${left + 1}px`
   }
   
@@ -24,3 +24,8 @@ document.addEventListener("keydown", function(e) {
     moveDodgerLeft();
   }
 });
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
